68004840e05ec65f693fe26b9a9d86cd7ed075a5
Matthias Leitl <a.dead.trousers@gmail.com>
2022-07-24 14:50:46 +0200

    Now the AccessibilityService has some functionality

--- app/src/main/java/at/co/are/hardwarekeymapper/HardwareKeyMapperService.kt
+++ app/src/main/java/at/co/are/hardwarekeymapper/HardwareKeyMapperService.kt
@@ -1,93 +1,173 @@
 package at.co.are.hardwarekeymapper
 
 import android.accessibilityservice.AccessibilityService
+import android.app.ActivityManager
+import android.content.Intent
 import android.hardware.display.DisplayManager
-import android.view.accessibility.AccessibilityEvent
-import androidx.preference.PreferenceManager
 import android.util.Log
 import android.view.Display
 import android.view.KeyEvent
 import android.view.Surface
+import android.view.accessibility.AccessibilityEvent
+import androidx.preference.PreferenceManager
 
 class HardwareKeyMapperService : AccessibilityService() {
+    companion object {
+        private const val TAG = "HardwareKeyMapperService"
+        lateinit var availableActionValues: Array<String>
+    }
+    init {
+        availableActionValues = SettingsActivity.appContext.resources.getStringArray(R.array.action_values)
+    }
+
     override fun onAccessibilityEvent(event: AccessibilityEvent) {}
+    override fun onInterrupt() {}
+
     public override fun onKeyEvent(event: KeyEvent): Boolean {
+// Quick Leave when canceled
+        if (event.isCanceled) return false
+// Quick Leave when not the proper action
         val action = event.action
-        if ((action != KeyEvent.ACTION_UP && action != KeyEvent.ACTION_DOWN) || event.isCanceled) {
-            return false
+        if (
+            action != KeyEvent.ACTION_UP
+            && action != KeyEvent.ACTION_DOWN
+        ) return false
+        Log.d(TAG,"KeyAction = "+action.toString())
+// Quick Leave when not the supported hardware key
+        val keyRes = when (event.keyCode) {
+            KeyEvent.KEYCODE_HOME -> R.string.key_key_home
+            KeyEvent.KEYCODE_BACK -> R.string.key_key_back
+            KeyEvent.KEYCODE_MENU -> R.string.key_key_menu
+            KeyEvent.KEYCODE_SEARCH -> R.string.key_key_search
+            KeyEvent.KEYCODE_APP_SWITCH -> R.string.key_key_app_switch
+            KeyEvent.KEYCODE_CAMERA -> R.string.key_key_camera
+            KeyEvent.KEYCODE_VOLUME_DOWN -> R.string.key_key_volume_down
+            KeyEvent.KEYCODE_VOLUME_UP -> R.string.key_key_volume_up
+            else -> return false
         }
+// Quick Leave when not the supported screen orientation
+        val orientationRes =
+            when ((getSystemService(DISPLAY_SERVICE) as DisplayManager).getDisplay(Display.DEFAULT_DISPLAY).rotation) {
+                Surface.ROTATION_0 -> {
+                    Log.d(TAG,getString(R.string.log_service_portrait_bottom))
+                    R.string.orientation_portrait_bottom
+                }
+                Surface.ROTATION_90 -> {
+                    Log.d(TAG,getString(R.string.log_service_landscape_right))
+                    R.string.orientation_landscape_right
+                }
+                Surface.ROTATION_180 -> {
+                    Log.d(TAG,getString(R.string.log_service_portrait_top))
+                    R.string.orientation_portrait_top
+                }
+                Surface.ROTATION_270 -> {
+                    Log.d(TAG,getString(R.string.log_service_landscape_left))
+                    R.string.orientation_landscape_left
+                }
+                else -> return false
+            }
+// Check the global settings first
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+// Check if key mapping is active
+        val keyName = getString(keyRes)
+        if (keyRes == R.string.key_key_menu || keyRes == R.string.key_key_camera) {
+            if (!sharedPreferences.getBoolean(keyName, false)) return false
+        } else {
+            if (!sharedPreferences.getBoolean(keyName, true)) return false
+        }
+// Check if orientation mapping is active
+        val orientationName = getString(orientationRes)
+        if (orientationRes == R.string.orientation_portrait_top) {
+            if (!sharedPreferences.getBoolean(orientationName, false)) return false
+        } else {
+            if (!sharedPreferences.getBoolean(orientationName, true)) return false
+        }
+
+        val orientationKey = orientationName + "_" + keyName
+// Check and run Overlay
+        val overlayApp = sharedPreferences.getString(
+            orientationKey + "_" + getString(R.string.key_overlay_app),
+            ""
+        )
+        val overlayIntentDown = sharedPreferences.getString(
+            orientationKey + "_" + getString(R.string.key_overlay_intent_down),
+            ""
+        )
+        val overlayIntentUp = sharedPreferences.getString(
+            orientationKey + "_" + getString(R.string.key_overlay_intent_up),
+            ""
+        )
+        if (executeOverlay(overlayApp, overlayIntentDown, overlayIntentUp, action, event.isLongPress)) return true
+// Check and run Actions
+        val actionShortPress = sharedPreferences.getString(
+            orientationKey + "_" + getString(R.string.key_action_short_press),
+            availableActionValues[0]
+        )
+        val actionLongPress = sharedPreferences.getString(
+            orientationKey + "_" + getString(R.string.key_action_long_press),
+            availableActionValues[0]
+        )
+        if (executeAction(actionShortPress, actionLongPress, action, event.isLongPress)) return true
+// Default if nothing was executed
         return false
-
-//To-DO: Redo the AccessibilityService
-        val keycode = event.keyCode
-        //if (action == KeyEvent.ACTION_UP) {
-        //    Toast.makeText(getApplicationContext(), "KeyCode = " + String.valueOf(keycode), Toast.LENGTH_LONG).show();
-        //}
-        if (action == KeyEvent.ACTION_DOWN && (keycode == KeyEvent.KEYCODE_BACK || keycode == KeyEvent.KEYCODE_APP_SWITCH)) {
-            // Don't want to send ACTION_DOWN to the apps as they may interpret it as a button
-            // press that's held down.
-            return !event.isLongPress
-        }
-        val pref = PreferenceManager.getDefaultSharedPreferences(this)
-        val backButtonActionPrefKey: String
-        val switchAppButtonActionPrefKey: String
-
-        when ((getSystemService(DISPLAY_SERVICE) as DisplayManager).getDisplay(Display.DEFAULT_DISPLAY).rotation) {
-            Surface.ROTATION_90 -> {
-                // button on the right.
-                backButtonActionPrefKey = "back_button_action_buttons_right"
-                switchAppButtonActionPrefKey = "switch_app_button_action_buttons_right"
-                Log.d(TAG, "Landscape 90 - buttons on the right")
-            }
-            Surface.ROTATION_180 -> {
-                // buttons on the top.
-                backButtonActionPrefKey = "back_button_action_buttons_left"
-                switchAppButtonActionPrefKey = "switch_app_button_action_buttons_left"
-                Log.d(TAG, "Portrait 180 - buttons on the top")
-            }
-            Surface.ROTATION_270 -> {
-                // buttons on the left.
-                backButtonActionPrefKey = "back_button_action_buttons_left"
-                switchAppButtonActionPrefKey = "switch_app_button_action_buttons_left"
-                Log.d(TAG, "Landscape 270 - buttons on the left")
-            }
-            else -> {
-                backButtonActionPrefKey = "back_button_action_portrait"
-                switchAppButtonActionPrefKey = "switch_app_button_action_portrait"
-                Log.d(TAG, "Portrait 0 - buttons on the bottom")
-            }
-        }
-        when (event.keyCode) {
-            KeyEvent.KEYCODE_BACK -> return if (pref.getString(
-                    backButtonActionPrefKey,
-                    BACK
-                ) == SWITCH_APP
-            ) {
-                performGlobalAction(GLOBAL_ACTION_RECENTS)
-                true
-            } else {
-                performGlobalAction(GLOBAL_ACTION_BACK)
-                true
-            }
-            KeyEvent.KEYCODE_APP_SWITCH -> if (pref.getString(
-                    switchAppButtonActionPrefKey,
-                    SWITCH_APP
-                ) == BACK
-            ) {
-                performGlobalAction(GLOBAL_ACTION_BACK)
-                return true
-            } else {
-                performGlobalAction(GLOBAL_ACTION_RECENTS)
+    }
+    private fun executeOverlay(overlayApp: String?, overlayIntentDown: String?, overlayIntentUp: String?, action: Int, isLongPress: Boolean): Boolean {
+        // The first in the list of RunningTasks is always the foreground task.
+        for (runningAppProcess in (getSystemService(ACTIVITY_SERVICE) as ActivityManager).runningAppProcesses) {
+            if (runningAppProcess.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                if (runningAppProcess.pkgList.equals(overlayApp)) {
+                    val actionIntent = when (action) {
+                        KeyEvent.ACTION_UP -> {
+                            if (overlayIntentUp != null && overlayIntentUp.isNotEmpty()) {
+                                Intent(overlayIntentUp)
+                            } else null
+                        }
+                        KeyEvent.ACTION_DOWN -> {
+                            if (overlayIntentDown != null && overlayIntentDown.isNotEmpty()) {
+                                if (overlayIntentUp == null || overlayIntentUp.isEmpty() || !isLongPress) {
+                                    Intent(overlayIntentDown)
+                                } else null
+                            } else null
+                        }
+                        else -> null
+                    }
+                    if (actionIntent != null) {
+                        actionIntent.`package` = overlayApp
+                        startActivity(actionIntent)
+                    }
+                    return true
+                }
             }
         }
         return false
     }
 
-    override fun onInterrupt() {}
-
-    companion object {
-        private const val TAG = "NavButtonRemapService"
-        private const val BACK = "0"
-        private const val SWITCH_APP = "1"
+    private fun executeAction(actionShortPress: String?, actionLongPress: String?, action: Int, isLongPress: Boolean): Boolean {
+        fun performSingleAction(action: Int): Boolean {
+            performGlobalAction(action)
+            return true
+        }
+        fun performEventAction(actionEvent: String?): Boolean {
+            return when(actionEvent) {
+                availableActionValues[0] -> false
+                availableActionValues[1] -> performSingleAction(GLOBAL_ACTION_BACK)
+                availableActionValues[2] -> performSingleAction(GLOBAL_ACTION_HOME)
+                availableActionValues[3] -> performSingleAction(GLOBAL_ACTION_RECENTS)
+                availableActionValues[4] -> performSingleAction(GLOBAL_ACTION_TAKE_SCREENSHOT)
+                availableActionValues[5] -> performSingleAction(GLOBAL_ACTION_LOCK_SCREEN)
+                availableActionValues[6] -> performSingleAction(GLOBAL_ACTION_POWER_DIALOG)
+                availableActionValues[7] -> performSingleAction(GLOBAL_ACTION_NOTIFICATIONS)
+                availableActionValues[8] -> performSingleAction(GLOBAL_ACTION_QUICK_SETTINGS)
+                availableActionValues[9] -> performSingleAction(GLOBAL_ACTION_TOGGLE_SPLIT_SCREEN)
+        //       availableActionValues[10] -> return performLocalAction(GLOBAL_ACTION_KEYCODE_HEADSETHOOK)
+                else -> false
+            }
+        }
+        if (action == KeyEvent.ACTION_UP && !isLongPress) {
+            return performEventAction(actionShortPress)
+        } else if (action == KeyEvent.ACTION_DOWN && isLongPress) {
+            return performEventAction(actionLongPress)
+        }
+        return false
     }
 }
