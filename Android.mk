6de20798a864cde2ec893b7b0f9a1fa15d0f18b1
Matthias Leitl <a.dead.trousers@gmail.com>
2023-11-17 19:39:21 +0100

    Propper AOSP build support

--- Android.mk
+++ Android.mk
@@ -1,17 +1,2 @@
 LOCAL_PATH:= $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_MODULE := HardwareKeyMapper
-
-LOCAL_CERTIFICATE := PRESIGNED
-
-LOCAL_SRC_FILES := app/release/app-release.apk
-
-LOCAL_MODULE_CLASS := APPS
-
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-
-include $(BUILD_PREBUILT)
+include $(call all-makefiles-under,$(LOCAL_PATH))
